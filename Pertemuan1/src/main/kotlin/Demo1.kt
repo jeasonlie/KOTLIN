ec39a26f8c005267dd504cde0d1c8724bf5a7566
jeasonlie <106128369+jeasonlie@users.noreply.github.com>
2023-09-12 09:09:13 +0700

    Pert1 Kotlin

---
+++ Pertemuan1/src/main/kotlin/Demo1.kt
@@ -0,0 +1,14 @@
+fun main(){
+    println("Hello World!")
+    println("Ngakak Cik")
+    println(1)
+    println(9.11)
+    println(3_500_300)
+    println(1/0.000_000_000_1)
+    println(69.0 % 5.0)
+    println("%.1f".format(6.0/7.0))
+    println("%.2f".format(6.0/7.0))
+    println("%.3f".format(6.0/7.0))
+    println("%.4f".format(6.0/7.0))
+    println(14 shl 2)
+}
