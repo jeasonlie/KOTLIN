4aa0dd7b8c2c6ff7b17dc98de71d1ef5d5e6de9e
jeasonlie <106128369+jeasonlie@users.noreply.github.com>
2023-09-16 10:02:40 +0700

    Create pert2.kt

---
+++ Pertemuan2/src/main/kotlin/pert2.kt
@@ -0,0 +1,13 @@
+fun main(){
+    println("Hello World")
+
+    //Contoh Komentar, shortcut = CTRL + /
+
+    /*Contoh komentar lebih
+      dari satu baris
+      contoh komentar lebih
+      dari 1 baris*/
+
+    var nama: String = "Jeason Lie"
+    println(nama)
+}
