6b0d719b306e4deb1b8a9e2a9f02e9dd04643700
jeasonlie <106128369+jeasonlie@users.noreply.github.com>
2023-09-16 10:24:57 +0700

    Update pert2.kt

--- Pertemuan2/src/main/kotlin/pert2.kt
+++ Pertemuan2/src/main/kotlin/pert2.kt
@@ -9,5 +9,19 @@ fun main(){
       dari 1 baris*/
 
     var nama: String = "Jeason Lie"
-    println(nama)
+    println("Nama : "+nama) //String concatenation
+    //nama = "8tat"
+    //println("Nama : $nama") //String interpolation
+
+    var tempatLahir = "Palembang"
+    println("Nama saya $nama dan saya lahir di $tempatLahir")
+
+    //var dan val
+    //var = nilai masih bisa diubah
+    //val (constanta) = nilai tidak bisa diubah
+    val usia = 22
+    //usia = 15 --> Val cannot be reassigned
+    var usiaSaatIni = 22
+    println("Tahun ini, saya $nama berusia $usiaSaatIni tahun")
+    println("Tahun depan, saya $nama berusia ${usiaSaatIni++} tahun")
 }
