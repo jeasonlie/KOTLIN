706ba0de43cbbcf10425f9cc4ac5afc3a862f96a
Michael Joy Clement <jeasonlie2207@mhs.mdp.ac.id>
2023-09-26 08:50:54 +0700

    Create kotlin.kt

---
+++ Pertemuan5/src/main/kotlin/kotlin.kt
@@ -0,0 +1,19 @@
+fun main(){
+    var sum = 0
+    for (i in 1 downTo 1 step 2){
+        sum += i
+    }
+    println(sum)
+
+    //label statements -> continue
+    sum = 0
+    for (row in 0 until 8){
+        if (row %2 == 0){
+            continue
+        }
+        for (column in 0 until 8){
+            sum += row * column
+        }
+    }
+    println(sum)
+}
