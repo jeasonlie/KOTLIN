23758be331c7ea9f2f7b14765163cbf98d985563
Ahmad Naufal <106128369+jeasonlie@users.noreply.github.com>
2023-09-30 10:42:46 +0700

    Update kotlin.kt

--- Pertemuan6/src/main/kotlin/kotlin.kt
+++ Pertemuan6/src/main/kotlin/kotlin.kt
@@ -1,3 +1,24 @@
 fun main(){
-    
+    cetakNamaSaya()
+    cetakPerkalian5(nilai = 10)
+    cetakPerkalian(pengali = 4, nilai = 2)
+
+    val (_, _) = multiplyAndDivide(number = 4, multiplier = 2)
 }
+
+fun cetakNamaSaya() {
+    TODO("Not yet implemented")
+    println("Nama saya adalah Jeason")
+}
+
+fun cetakPerkalian(pengali: Int, nilai: Int = 1) {
+    TODO("Not yet implemented")
+    println("$pengali * $nilai = ${pengali * nilai}")
+}
+
+fun cetakPerkalian5(nilai: Int) {
+    TODO("Not yet implemented")
+    println()
+}
+
+fun multiplyAndDivide(number: Int, multiplier: Int) = number * multiplier
